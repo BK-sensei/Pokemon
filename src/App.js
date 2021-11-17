b12b475c5058ce1b34fd90e6b75767c660bcde12
becca <rebecca.kaci@gmail.com>
2021-11-17 16:29:30 +0100

    ajoute d'une route erreur404 et navigation entre les routes ok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+
 class App extends Component {
   render() {
     return (
       
       <BrowserRouter>
         <Switch>
-          <Route />
-          <Route />
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="*" component={NotFound}/>
         </Switch>
       </BrowserRouter>
     );
