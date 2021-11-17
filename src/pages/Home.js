ab842de84e6ec5f3b069a9fa8ecd7ebb9875a487
becca <rebecca.kaci@gmail.com>
2021-11-17 17:09:45 +0100

    création d'un state pokemon et sa fonction de mise à jour setPokemon en utilisant le hook useState() initialisé avec la valeur null et d'un hook useEffect pour récupérer les informations du premier Pokémon et le stocker dans le state avec la fonction setPokemon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 
 function Home() {
+
+    const [pokemon, setPokemon] = useState(null)
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon/1')
+        .then (response => response.json())
+        .then (result => {
+            setPokemon([result])
+        })
+    },[])
+    console.log(pokemon);
     return (
-        <div>
-            HOME
-        </div>
-    );
+        <h1>HOME</h1>
+    )
 }
 
-export default Home;
+export default Home
